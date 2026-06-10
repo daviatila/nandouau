3bee2559c1b73ba7089393625c829ab9ef49c595
Davi Atila <daviatila15@gmail.com>
2026-06-10 13:50:53 -0300

    Atualização

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Artist from "./pages/Artist";
-import JaSabia from "./pages/JaSabia";
-import HistoriaJaSabia from "./pages/HistoriaJaSabia";
 import Gallery from "./pages/Gallery";
 import FloatingMenu from "./components/FloatingMenu";
 import Socials from "./pages/Socials";
@@ -18,10 +16,6 @@ function App() {
         <Route path="/" element={<Home />} />
 
         <Route path="/artista" element={<Artist />} />
-
-        <Route path="/ja-sabia" element={<JaSabia />} />
-
-        <Route path="/historia-ja-sabia" element={<HistoriaJaSabia />} />
 
         <Route path="/galeria" element={<Gallery />} />
 
